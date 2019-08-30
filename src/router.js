7e6b51deb93083de8192ab28e47ae50f2c404baa
image677 <wy17603@163.com>
2019-08-30 10:43:26 +0800

    update

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Layout from './components/Layout.vue'
 
 Vue.use(Router)
 
@@ -20,6 +21,36 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import(/* webpackChunkName: "about" */ './views/Login.vue')
+    },
+    {
+      path: '/table',
+      // name: 'table',
+      component: Layout,
+      children: [
+        {
+          path: '/table',
+          name: 'table',
+          component: () => import('./views/Table.vue')
+          // meta: { title: 'Table', icon: 'table' }
+        }
+      ]
     }
+    // {
+    //   path: '/form',
+    //   component: Layout,
+    //   children: [
+    //     {
+    //       path: 'index',
+    //       name: 'Form',
+    //       component: () => import('@/views/form/index'),
+    //       meta: { title: 'Form', icon: 'form' }
+    //     }
+    //   ]
+    // },
   ]
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <HelloWorld />
+  </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '../components/HelloWorld'
 
 export default {
-  name: 'home',
   components: {
     HelloWorld
   }
