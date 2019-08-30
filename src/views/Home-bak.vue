397441025a25599d0439549079f61b291ec384b0
image677 <wy17603@163.com>
2019-08-30 10:45:49 +0800

    add file

---
+++ src/views/Home-bak.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <HelloWorld />
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from '../components/HelloWorld'
+
+export default {
+  components: {
+    HelloWorld
+  }
+}
+</script>
