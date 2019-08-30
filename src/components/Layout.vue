c55ce5453f4d35dd48f271812fac872a16f1ebff
image677 <wy17603@163.com>
2019-08-30 10:34:27 +0800

    add layout,login,vuetify

---
+++ src/components/Layout.vue
@@ -0,0 +1,178 @@
+<template>
+    <v-app>
+      <v-navigation-drawer app>
+         <v-card
+            class="mx-auto"
+            width="300"
+          >
+            <v-list>
+              <v-list-item link>
+                <v-list-item-icon>
+                  <v-icon>home</v-icon>
+                </v-list-item-icon>
+
+                <v-list-item-title>Home</v-list-item-title>
+              </v-list-item>
+
+              <v-list-group
+                prepend-icon="account_circle"
+                value="true"
+              >
+                <template v-slot:activator>
+                  <v-list-item-title>Users</v-list-item-title>
+                </template>
+
+                <v-list-group
+                  no-action
+                  sub-group
+                  value="true"
+                >
+                  <template v-slot:activator>
+                    <v-list-item-content>
+                      <v-list-item-title>Admin</v-list-item-title>
+                    </v-list-item-content>
+                  </template>
+
+                  <v-list-item
+                    v-for="(admin, i) in admins"
+                    :key="i"
+                    link
+                  >
+                    <v-list-item-title v-text="admin[0]"></v-list-item-title>
+                    <v-list-item-icon>
+                      <v-icon v-text="admin[1]"></v-icon>
+                    </v-list-item-icon>
+                  </v-list-item>
+                </v-list-group>
+
+                <v-list-group
+                  sub-group
+                  no-action
+                >
+                  <template v-slot:activator>
+                    <v-list-item-content>
+                      <v-list-item-title>Actions</v-list-item-title>
+                    </v-list-item-content>
+                  </template>
+                  <v-list-item
+                    v-for="(crud, i) in cruds"
+                    :key="i"
+                  >
+                    <v-list-item-title v-text="crud[0]"></v-list-item-title>
+                    <v-list-item-action>
+                      <v-icon v-text="crud[1]"></v-icon>
+                    </v-list-item-action>
+                  </v-list-item>
+                </v-list-group>
+              </v-list-group>
+            </v-list>
+          </v-card>
+      </v-navigation-drawer>
+
+      <v-app-bar app>
+        <div style="width: 100%">
+            Material Design
+            <div class="float-right">
+                <v-avatar>
+                  <img src="https://vuetifyjs.com/apple-touch-icon-180x180.png" alt="avatar">
+                </v-avatar>
+                <span>superuser</span>
+                <!-- <v-btn text icon color="black" @click="darkTheme">
+                    <v-icon>nights_stay</v-icon>
+                </v-btn>
+                <v-btn text icon color="amber" @click="lightTheme">
+                    <v-icon>wb_sunny</v-icon>
+                </v-btn>
+                <v-btn text icon color="red">
+                  <v-icon>power_settings_new</v-icon>
+                </v-btn> -->
+
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on }">
+                    <v-btn text icon color="black" @click="darkTheme" v-on="on">
+                        <v-icon>nights_stay</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>夜间模式</span>
+                </v-tooltip>
+
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on }">
+                    <v-btn text icon color="amber" @click="lightTheme" v-on="on">
+                        <v-icon>wb_sunny</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>日间模式</span>
+                </v-tooltip>
+
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on }">
+                    <v-btn text icon color="red" v-on="on">
+                      <v-icon>power_settings_new</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>注销登录</span>
+                </v-tooltip>
+            </div>
+        </div>
+      </v-app-bar>
+
+      <!-- Sizes your content based upon application components -->
+      <v-content>
+
+        <!-- Provides the application the proper gutter -->
+        <v-container fluid>
+            <!-- <v-breadcrumbs :items="items" divider="-"></v-breadcrumbs> -->
+          <!-- If using vue-router -->
+          <router-view></router-view>
+        </v-container>
+      </v-content>
+
+      <v-footer app>
+        <p style="text-align: center;width: 100%">Copyright © 2019</p>
+      </v-footer>
+    </v-app>
+</template>
+
+<script>
+export default {
+  name: 'Layout',
+  data: () => ({
+    items: [
+      {
+        text: 'Dashboard',
+        disabled: false,
+        href: 'breadcrumbs_dashboard'
+      },
+      {
+        text: 'Link 1',
+        disabled: false,
+        href: 'about'
+      },
+      {
+        text: 'Link 2',
+        disabled: true,
+        href: 'breadcrumbs_link_2'
+      }
+    ],
+    admins: [
+      ['Management', 'people_outline'],
+      ['Settings', 'settings']
+    ],
+    cruds: [
+      ['Create', 'add'],
+      ['Read', 'insert_drive_file'],
+      ['Update', 'update'],
+      ['Delete', 'delete']
+    ]
+  }),
+  methods: {
+    lightTheme () {
+      this.$vuetify.theme.isDark = false
+    },
+    darkTheme () {
+      this.$vuetify.theme.isDark = true
+    }
+  }
+}
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,135 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+            md="4"
+          >
+            <v-form v-model="valid">
+            <v-card class="elevation-12">
+              <v-toolbar
+                color="primary"
+                dark
+                flat
+              >
+                <v-toolbar-title>Material Design</v-toolbar-title>
+                <div class="flex-grow-1"></div>
+              </v-toolbar>
+              <v-card-text>
+                <v-form ref="form">
+                  <v-text-field
+                    label="用户名"
+                    prepend-icon="account_circle"
+                    type="text"
+                    v-model="username"
+                    :rules="nameRules"
+                  ></v-text-field>
+
+                  <v-text-field
+                    label="密码"
+                    prepend-icon="lock"
+                    :append-icon="show1 ? 'visibility' : 'visibility_off'"
+                    :type="show1 ? 'text' : 'password'"
+                    v-model="password"
+                    :rules="passRules"
+                    @click:append="show1 = !show1"
+                  ></v-text-field>
+
+                  <v-text-field
+                    label="验证码"
+                    prepend-icon="verified_user"
+                    type="text"
+                    v-model="verifyCode"
+                    :rules="codeRules"
+                  ></v-text-field>
+                  <div style="text-align: center;">
+                    <img alt="验证码" src="../assets/code.jpg" style="vertical-align: middle;">
+                    &nbsp;<v-btn text small>换一张</v-btn>
+                  </div>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <div class="flex-grow-1"></div>
+                <v-btn color="primary" @click="doSubmit">登录</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-form>
+          </v-col>
+        </v-row>
+      </v-container>
+      <v-snackbar
+      v-model="toast"
+      :timeout=1500
+      top
+    >
+      {{ msgtext }}
+      <v-icon color="red">error_outline</v-icon>
+      <!-- <v-btn
+        color="pink"
+        text
+        @click="toast = false"
+      >
+        <v-icon>close</v-icon>
+      </v-btn> -->
+    </v-snackbar>
+    </v-content>
+  </v-app>
+</template>
+<style scoped>
+  /*#appbg{background-image: url('../assets/login-bg2.png');background-repeat: no-repeat;background-size: cover;height: 100%}*/
+  #app{background-image: url('../assets/login-bg2.png');background-repeat: no-repeat;background-size: cover;}
+</style>
+
+<script>
+// import MaterialImage from 'material-image';
+// const myDom = document.querySelector('#app');
+// new MaterialImage({
+//  el: myDom,
+// });
+export default {
+  data: () => ({
+    msgtext: '',
+    show1: false,
+    toast: false,
+    valid: false,
+    username: '',
+    password: '',
+    verifyCode: '',
+    nameRules: [
+      v => !!v || '请输入用户名！',
+      v => v.length <= 10 || '用户名不超过10位！'
+    ],
+    passRules: [
+      v => !!v || '请输入密码！',
+      v => v.length <= 15 || '密码不超过15位！'
+    ],
+    codeRules: [
+      v => !!v || '请输入验证码！',
+      v => v.length === 6 || '验证码为6位数！'
+    ]
+  }),
+  methods: {
+    doSubmit () {
+      if (this.$refs.form.validate() === false) {
+        this.snackbar = true
+        if (this.username && this.password && this.verifyCode) {
+          this.msgtext = '用户名或密码错误'
+          this.toast = true
+        }
+      } else {
+        // console.log('ok')
+        this.$router.push('table')
+      }
+    }
+  }
+}
+</script>
